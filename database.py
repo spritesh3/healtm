0d7152d99ddd21b4b6fa8f0e8c34c2be73837f65
spritesh3 <93711755+spritesh3@users.noreply.github.com>
2026-02-27 03:41:14 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
-st.write("DB URL Loaded:", bool(st.secrets.get("DATABASE_URL")))
 import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Debug (temporary)
+st.write("DB URL Loaded:", bool(st.secrets.get("DATABASE_URL")))
 
 DATABASE_URL = st.secrets["DATABASE_URL"]
 
