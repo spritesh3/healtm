dcd9ecc27dacf66980cf570abcb5df5570d06ef4
spritesh3 <93711755+spritesh3@users.noreply.github.com>
2026-02-27 03:37:19 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,16 @@
+st.write("DB URL Loaded:", bool(st.secrets.get("DATABASE_URL")))
+import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+DATABASE_URL = st.secrets["DATABASE_URL"]
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
